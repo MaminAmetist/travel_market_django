dcb7f3af8f324c8f8ff9abc015c82162cd208d4e
Анна Нездулькина <anna.nezdulkina@mail.ru>
2025-04-19 14:41:58 +0700

    Update settings.py

--- market_prj/settings.py
+++ market_prj/settings.py
@@ -156,13 +156,13 @@ SOCIAL_AUTH_PIPELINE = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
